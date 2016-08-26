e687c001c12d5c5d0c7b348aba5b29c476381cbb
cyril ferte <cyril@fertec.fr>
2016-08-26 04:27:32 +0000

    pret a upload add category

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :upvotes
   validates :name, presence: true, uniqueness: true
+  validates :tagline, presence: true
   validates :url, presence: true, uniqueness: true
-  validates :category, inclusion: { in: %w(Tech Schema_2D Process_Simulation CFD Sizing Mathematic Biology Chemistry DATA_Analytics Design_3D Material Analyse_tools IT Energy Education Food),
+  validates :category, presence: true, inclusion: { in: %w(Tech Schema_2D Process_Simulation CFD Sizing Mathematic Biology Chemistry DATA_Analytics Design_3D Material Analyse_tools IT Energy Education Food),
     message: "%{value} is not a valid category" }
 end
